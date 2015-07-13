03f963cd97ee563b6c5e19e4b25f75bb853e25fb
benjipott <pott.benjamin@gmail.com>
2015-07-13 10:30:41 -0700

    Add redux hot loader sample

---
+++ src/ApiClient.js
@@ -0,0 +1,44 @@
+/*global __SERVER__*/
+import superagent from 'superagent';
+import config from 'config';
+
+function formatUrl(path) {
+  let adjustedPath = path[0] !== '/' ? '/' + path : path;
+  if (__SERVER__) {
+    // Prepend host and port of the API server to the path.
+    return 'http://localhost:' + config.apiPort + adjustedPath;
+  }
+  // Prepend `/api` to relative URL, to proxy to API server.
+  return '/api' + adjustedPath;
+}
+
+export default class ApiClient {
+  constructor(req) {
+    ['get', 'post', 'put', 'patch', 'del'].
+      forEach((method) => {
+        this[method] = (path, options) => {
+          return new Promise((resolve, reject) => {
+            let request = superagent[method](formatUrl(path));
+            if (options && options.params) {
+              request.query(options.params);
+            }
+            if (__SERVER__) {
+              if(req.get('cookie')){
+                request.set('cookie', req.get('cookie'));
+              }
+            }
+            if (options && options.data) {
+              request.send(options.data);
+            }
+            request.end((err, res) => {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(res.body);
+              }
+            });
+          });
+        };
+      });
+  }
+}
---
+++ src/components/MiniInfoBar.js
@@ -0,0 +1,40 @@
+/*global __CLIENT__*/
+import React, {PropTypes} from 'react';
+import {connect} from 'redux/react';
+if (__CLIENT__) {
+  require('./InfoBar.scss');
+}
+
+class MiniInfoBar {
+  static propTypes = {
+    time: PropTypes.number
+  }
+
+  render() {
+    const {time} = this.props;
+    return (
+      <div className="mini-info-bar">
+        The info bar was last loaded at
+        {' '}
+        <span>{time && new Date(time).toString()}</span>
+      </div>
+    );
+  }
+}
+
+@connect(state => ({
+  time: state.info.data.time
+}))
+export default
+class MiniInfoBarContainer {
+  static propTypes = {
+    time: PropTypes.number,
+    dispatch: PropTypes.func.isRequired
+  }
+
+  render() {
+    const { time, dispatch } = this.props;
+    return <MiniInfoBar time={time}/>;
+    // no bindActionCreators() because this component is display-only
+  }
+}
