aeaad39088d52237afa9b0a0f7ae2ccde02ece31
John Archambault <jarchambault@libertyglobal.com>
2015-08-03 02:24:08 +0200

    supprimer le sso pop-up pendant abonnement ou se connecter

--- src/js/components/Welcome/Welcome.jsx
+++ src/js/components/Welcome/Welcome.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
 import * as UserActionCreators from '../../actions/user';
 import { connect } from 'react-redux';
-import SignupButton from './../User/SignupButton';
-import SigninButton from './../User/SigninButton';
+import config from '../../../../config/client';
 
 var Welcome = React.createClass ({
 
-	render: function() {
-		const {
-			props: {
-				User
-				}
-			} = this;
+	showSigninLock: function() {
 
+		this.props.lock.show(
+			{
+				dict: 'fr',
+				connections: ['Username-Password-Authentication', 'facebook'],
+				socialBigButtons: true,
+				disableSignupAction: true,
+				rememberLastLogin: false
+			}
+		);
+	},
+
+	showSignupLock: function() {
+
+		this.props.lock.showSignup(
+			{
+				dict: 'fr',
+				connections: ['Username-Password-Authentication', 'facebook'],
+				socialBigButtons: true
+			}
+		);
+	},
+
+	render: function() {
 		return (
 			<div>
-				<SignupButton />
-				<SigninButton />
+				<h3>In the welcome  page</h3>
+				<div className="btn-group navbar-collapse collapse navbar-left">
+					<button type="button" className="btn btn-user btn-default dropdown-toggle" data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false" onClick={this.showSignupLock}>abonnez-vous
+					</button>
+				</div>
+
+				<div className="btn-group navbar-collapse collapse navbar-left">
+					<button type="button" className="btn btn-user btn-default dropdown-toggle" data-toggle="dropdown"
+						aria-haspopup="true"
+						aria-expanded="false" onClick={this.showSigninLock}>se connecter
+					</button>
+				</div>
 			</div>
 		);
 	}
