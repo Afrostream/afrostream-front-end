144ba7499379da88e48131ba6d40f5e27188ff25
John Archambault <jarchambault@libertyglobal.com>
2015-08-20 17:11:03 +0200

    footer de les pages daccueil, inscription

--- src/js/components/Welcome/ReturningUser.jsx
+++ src/js/components/Welcome/ReturningUser.jsx
@@ -3,77 +3,83 @@ import { Link } from 'react-router';
 import Welcome from './Welcome';
 import Browse from './Browse';
 import AfrostreamMonthlyMessage from './AfrostreamMonthlyMessage';
+import WelcomeFooter from './WelcomeComponents/WelcomeFooter';
 import Payment from './Payment';
 import Application from '../Application';
 
-var ReturningUser = React.createClass({
+var ReturningUser = React.createClass ({
 
-  getInitialState: function () {
+	getInitialState: function() {
 
-    return {
-      profile: null,
-      tokenExpired: null
-    }
-  },
+		return {
+			profile: null,
+			tokenExpired: null
+		}
+	},
 
-  componentDidMount: function () {
+	componentDidMount: function() {
 
-    this.props.lock.getProfile(this.props.idToken, function (err, profile) {
-      if (err) {
+		this.props.lock.getProfile(this.props.idToken, function (err, profile) {
+			if (err) {
 
-        console.log("*** Error loading the profile in ReturningUser.jsx - most likely the token has expired ***", err);
-        localStorage.removeItem('afroToken');
+				console.log("*** Error loading the profile in ReturningUser.jsx - most likely the token has expired ***", err);
+				localStorage.removeItem('afroToken');
 
-        this.setState({
-          tokenExpired: true
-        });
-      }
-      this.setState({profile: profile});
+				this.setState({
+					tokenExpired: true
+				});
+			}
+			this.setState({profile: profile});
 
-    }.bind(this));
-  },
+		}.bind(this));
+	},
 
-  render: function () {
+	render: function() {
 
-    const {
-      props: { episode }
-      } = this;
+		const {
+			props: { episode }
+		} = this;
 
-    if (this.state.profile) {
-      console.log('*** here is the user profile ***');
-      console.log(this.state.profile);
-      console.log('*** end of the profile info ***');
-      console.log(this.state.profile.paymentStatus);
-      console.log('*** end of payment status ***');
+		if (this.state.profile) {
 
-      if ((typeof this.state.profile.paymentStatus !== 'undefined')
-        && (this.state.profile.paymentStatus === true)
-        && (typeof this.state.profile.planCode !== 'undefined')
-        && (this.state.profile.planCode !== 'afrostreammonthly')) {
+			if ( (typeof this.state.profile.paymentStatus !== 'undefined')
+				&& (this.state.profile.paymentStatus === true)
+				&& (typeof this.state.profile.planCode !== 'undefined')
+				&& (this.state.profile.planCode !== 'afrostreammonthly') ) {
 
-        //return (<Browse lock={this.props.lock} idToken={this.props.idToken} profile={this.state.profile}  />);
-        return (<Application paymentStatus={true} children={this.props.children}/>);
+				//debugger;
+				//return (<Browse lock={this.props.lock} idToken={this.props.idToken} profile={this.state.profile}  />);
+				return (<Application paymentStatus={true} children={this.props.children} />);
 
-      } else if ((typeof this.state.profile.paymentStatus !== 'undefined')
-        && (this.state.profile.paymentStatus === true)
-        && (typeof this.state.profile.planCode !== 'undefined')
-        && (this.state.profile.planCode === 'afrostreammonthly')) {
+			} else if((typeof this.state.profile.paymentStatus !== 'undefined')
+				&& (this.state.profile.paymentStatus === true)
+				&& (typeof this.state.profile.planCode !== 'undefined')
+				&& (this.state.profile.planCode === 'afrostreammonthly')) {
 
-        return (
-          <AfrostreamMonthlyMessage lock={this.props.lock} idToken={this.props.idToken} profile={this.state.profile}/>);
+				return(
+					<div>
+						<AfrostreamMonthlyMessage lock={this.props.lock}  idToken={this.props.idToken} profile={this.state.profile} />
+						<WelcomeFooter />
+					</div>
+				);
 
-      } else {
+			} else {
 
-        return (<Payment lock={this.props.lock} idToken={this.props.idToken} profile={this.state.profile}/>);
-      }
-    } else if (this.state.tokenExpired === true) {
+				return (
+					<div>
+						<Payment lock={this.props.lock} idToken={this.props.idToken} profile={this.state.profile} />
+						<WelcomeFooter />
+					</div>
+				);
+			}
+		} else if (this.state.tokenExpired === true) {
 
-      return (<Welcome lock={this.props.lock}/>);
-    } else {
+			return(<Welcome lock={this.props.lock} />);
+		} else {
 
-      return (<div></div>);
-    }
-  }
+			return (<div></div>);
+		}
+	}
 });
 
 module.exports = ReturningUser;
