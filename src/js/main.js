257aa4c898780ac30abf919d2fed4ea7d70cff07
benjipott <pott.benjamin@gmail.com>
2015-08-21 17:44:30 +0200

    force api endpoint

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,7 @@ const api = createAPI(
   ({ method, headers = {}, pathname = '', query = {}, body = {} }) => {
     pathname = pathname.replace(new RegExp(`^${apiClient.urlPrefix}`), '');
     var url = `${apiClient.urlPrefix}${pathname}`;
+    console.log('api url', url)
 
     return request(method, url)
       .query(qs.stringify(query))
