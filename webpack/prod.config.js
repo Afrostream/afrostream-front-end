78c9ade3b8b765293f260f74ced9899e61356a4c
benjipott <pott.benjamin@gmail.com>
2015-08-21 16:16:18 +0200

    remove consolelog

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -33,8 +33,10 @@ const prodConfig = merge({}, webpackConfig, {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.OccurenceOrderPlugin(),
     new webpack.optimize.UglifyJsPlugin({
+      output: {comments: false},
       compress: {
-        warnings: false
+        warnings: false,
+        drop_console: true
       }
     })
     //FIXME Replace mock remover for staging/production
