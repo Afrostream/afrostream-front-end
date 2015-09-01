5ceb7f1a83e2f541f01e22f3ae221b0bccca7443
benjipott <pott.benjamin@gmail.com>
2015-09-01 14:44:09 +0200

    fix player

--- src/js/components/Player/PlayerPage.jsx
+++ src/js/components/Player/PlayerPage.jsx
@@ -3,11 +3,14 @@ import { prepareRoute } from '../../decorators';
 import * as VideoActionCreators from '../../actions/video';
 import * as MovieActionCreators from '../../actions/movie';
 import * as SeasonActionCreators from '../../actions/season';
+import * as EventActionCreators from '../../actions/event';
+
 import PlayerComponent from './PlayerComponent';
 
 @prepareRoute(async function ({ store, params: { movieId,movieSlug,seasonId,seasonSlug,episodeId,episodeSlug, videoId } }) {
   console.log(movieId, movieSlug, seasonId, seasonSlug, episodeId, episodeSlug, videoId);
   return await * [
+      store.dispatch(EventActionCreators.pinHeader(false)),
       store.dispatch(VideoActionCreators.getVideo(videoId)),
       store.dispatch(MovieActionCreators.getMovie(movieId))
     ];
