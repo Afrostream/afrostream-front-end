9543d80e5694f994423fce6ac7762db915573fc0
John Archambault <jarchambault@libertyglobal.com>
2015-09-10 00:43:32 +0200

    payment page ssl

--- src/js/components/Payment/PaymentPage.jsx
+++ src/js/components/Payment/PaymentPage.jsx
@@ -13,8 +13,25 @@ if (process.env.BROWSER) {
     ];
 }) class PaymentPage extends React.Component {
 
+  /**
+   * Get the value of a querystring
+   * @param  {String} field The field to get the value of
+   * @param  {String} url   The URL to get the value from (optional)
+   * @return {String}       The field value
+   */
+  getQueryString = function (field, url) {
+    var href = url ? url : window.location.href;
+    var reg = new RegExp( '[?&]' + field + '=([^&#]*)', 'i' );
+    var string = reg.exec(href);
+    return string ? string[1] : null;
+  }
+
   render() {
 
+    localStorage.setItem('afroToken', this.getQueryString('afroToken'));
+    localStorage.setItem('afro_token', this.getQueryString('afro_token'));
+    localStorage.setItem('johnarchHere', 'yoAdrian')
+    
     return (<SelectPlan />);
   }
 }
