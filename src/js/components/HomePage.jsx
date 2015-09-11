956ebb6ac30135295428ca5a308e24cf7d5c408d
John Archambault <jarchambault@libertyglobal.com>
2015-09-11 06:29:12 +0200

    payment page ssl

--- src/js/components/HomePage.jsx
+++ src/js/components/HomePage.jsx
@@ -1,23 +1,51 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import * as UserActionCreators from '../actions/user';
 import WelcomePage from './Welcome/WelcomePage';
 import BrowsePage from './Browse/BrowsePage';
 import Redirect from './Redirect/Redirect';
 import AfrostreamMonthlyMessage from './Welcome/AfrostreamMonthlyMessage';
 import Spinner from './Spinner/Spinner';
+import { canUseDOM } from 'react/lib/ExecutionEnvironment';
 
 @connect(({ User }) => ({User})) class HomePage extends React.Component {
+
+  getQueryString(field, url) {
+    if (canUseDOM) {
+      let href = url ? url : window.location.href;
+      let reg = new RegExp('[?&]' + field + '=([^&#]*)', 'i');
+      let string = reg.exec(href);
+      return string ? string[1] : null;
+    }
+    return null;
+  }
+
+  logout() {
+    const {
+      props: {
+        dispatch
+        }
+      } = this;
+
+    dispatch(UserActionCreators.logOut());
+  }
+
   render() {
     const { props: { User ,children} } = this;
     const token = User.get('token');
     const user = User.get('user');
+    const paymentStatus = this.getQueryString('payment_success');
 
     if (token) {
       if (!user) {
         return (<Spinner />);
       }
-      else if (!user.get('planCode')) {
+      else if (!user.get('planCode') && paymentStatus !== 'true') {
         return (<Redirect />)
+      }
+      else if (!user.get('planCode') && paymentStatus === 'true') {
+        //return (<Redirect />)
+        //this::logout;
       }
       else if (user.get('planCode') === 'afrostreammonthly') {
         return ( <AfrostreamMonthlyMessage />)
