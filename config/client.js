4d6513eac55feea76d7c6c2c81c8fe1cc101e8db
benjipott <pott.benjamin@gmail.com>
2015-09-28 17:25:23 +0200

    add switch bitrate menu

--- config/client.js
+++ config/client.js
@@ -71,7 +71,7 @@ export default {
     "language": "fr",
     "flash": {
       "params": {
-        "wmode": "direct"
+        "wmode": "opaque"
       }
     },
     "languages": {
@@ -106,7 +106,7 @@ export default {
       "ID_CLIENT": process.env.STREAMROOT_CLIENT_ID || 'ry-0gzuhlor',
       "TRACKER_URL": process.env.STREAMROOT_TRACKER_URL || ''
     },
-    "techOrder": ["hls", "html5", "dash", "flash", "streamroot", "srflash"],
+    "techOrder": ["html5", "dash", "hls", "flash", "streamroot", "srflash"],
     "plugins": {
       "chromecast": {
         "appId": process.env.CHROMECAST_ID || '',
