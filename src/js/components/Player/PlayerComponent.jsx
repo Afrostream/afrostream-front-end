01fd2d006ee696f7d88b3687f0cbdd80a90aeb8c
benjipott <pott.benjamin@gmail.com>
2015-11-12 18:18:28 +0100

    fix flash component

--- src/js/components/Player/PlayerComponent.jsx
+++ src/js/components/Player/PlayerComponent.jsx
@@ -226,19 +226,11 @@ class PlayerComponent extends React.Component {
           }
 
           let player = videojs('afrostream-player', playerData).ready(function () {
-              //var allTracks = this.textTracks() || []; // get list of tracks
-              //_.forEach(allTracks, function (track) {
-              //  let lang = track.language || track.language_;
-              //  track.mode = lang === 'fr' ? 'showing' : 'hidden'; // show this track
-              //});
               var allTracks = this.textTracks() || []; // get list of tracks
-              let trackFr = _.find(allTracks, function (track) {
-                return track.language === 'fr';
+              _.forEach(allTracks, function (track) {
+                let lang = track.language || track.language_;
+                track.mode = lang === 'fr' ? 'showing' : 'hidden'; // show this track
               });
-              if (trackFr) {
-                console.log(trackFr);
-                trackFr.mode = 'showing'; // show this track
-              }
             }
           );
           player.on('pause', this.setDurationInfo.bind(this));
