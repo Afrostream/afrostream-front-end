56dbeb5e2a36c88bc300ff77ed4d277b8a47fba3
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-13 14:32:41 +0100

    feature geolocation: wording

--- src/js/components/Modal/ModalGeoWall.jsx
+++ src/js/components/Modal/ModalGeoWall.jsx
@@ -26,14 +26,14 @@ class ModalGeoWall extends React.Component {
       <div className="modalDialog geoWall">
         <div>
           <a onClick={::this.handleClose} title="Close" className="close">X</a>
-          <p className="center bold">Sorry !<br/>
-            Afrostream is not yet available in your country</p>
+          <p className="center bold">Coming Soon</p>
+          <p className="formText">We are not yet available in your country.</p>
           <p className="formText">
-            Enter your email to stay informed :
+            Enter your details below and be the first to get notified when we launch there :
           </p>
           <p>
             <form onSubmit={::this.handleSubmit}>
-              <input type="text" name="email" ref="email" placeholder="Email"/>
+              <input type="text" name="email" ref="email" placeholder="example@address.com"/>
               <button type="submit" className="btn btn-ok btn-default pull-right">OK</button>
             </form>
           </p>
