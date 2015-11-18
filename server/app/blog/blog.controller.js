3b1dbda8a4b1e89b861977b68fe5c688b19b5ca6
Marc Dassonneville <marc.dassonneville@gmail.com>
2015-11-18 14:49:45 +0100

    /blog/{slug}.html is now redirected to /blog/postUUID/slug

--- server/app/blog/blog.controller.js
+++ server/app/blog/blog.controller.js
@@ -1,6 +1,40 @@
 'use strict';
 
+import request from 'superagent';
+import config from '../../../config';
+
+const { apiClient: { protocol, authority }} = config;
+
+/**
+ * redirecting route
+ *    /blog/Afrostream-24-hour-love.html
+ * to
+ *    /blog/761b1042-7d0a-40a9-83ac-1b2ee5918b24/Afrostream-24-hour-love
+ */
+export function redirect(req, res, next) {
+  const url = protocol + '://' + authority + '/api/posts';
+  const slug = req.params.slug;
+
+  request
+    .get(url)
+    .query({slug:slug})
+    .type('json')
+    .end((err, response) => {
+      if (err) {
+        // FIXME: test the global error handler.
+        return next(err);
+      }
+      if (!Array.isArray(response.body) ||
+        response.body.length === 0 ||
+        !response.body[0]._id) {
+        // FIXME: design the 404.
+        return res.status(404).send('post not found');
+      }
+      res.redirect('/blog/'+response.body[0]._id+'/'+slug);
+    });
+};
+
 export function show(req, res, next) {
-  // FIXME: read in the api
+  // FIXME: display the post ?
   res.send(req.params.postUUID);
 };
--- server/app/blog/index.js
+++ server/app/blog/index.js
@@ -2,25 +2,11 @@
 
 import express from 'express';
 
-import { show } from './blog.controller';
+import { show, redirect } from './blog.controller';
 
 const router = express.Router();
 
-/**
- * redirecting route
- *    /blog/Afrostream-24-hour-love.html
- * to
- *    /blog/761b1042-7d0a-40a9-83ac-1b2ee5918b24/Afrostream-24-hour-love
- */
-router.use(function (req, res, next) {
-  console.log('middleware !' + req.url);
-  const matches = req.url.match(/^\/([^/]+)\.html$/);
-  if (matches && matches[1]) {
-
-  }
-  next();
-});
-
+router.get('/:slug.html', redirect);
 router.get('/:postUUID/*', show);
 
 module.exports = router;
