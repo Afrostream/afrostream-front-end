87a911729aa57390e0ee0aa05359ba935525f1a3
benjipott <pott.benjamin@gmail.com>
2015-12-10 07:36:28 +0100

    remove transition && add shadow to player txt

--- src/js/components/Movies/MoviesCategorySlider.jsx
+++ src/js/components/Movies/MoviesCategorySlider.jsx
@@ -5,11 +5,6 @@ import config from '../../../../config';
 import Slider from '../Slider/Slider';
 import LazyLoader from './LazyLoader';
 
-if (canUseDOM) {
-  require('gsap');
-  var {TimelineMax,TweenMax,Sine} = window.GreenSockGlobals;
-}
-
 if (process.env.BROWSER) {
   require('./MoviesList.less');
 }
@@ -19,48 +14,6 @@ class MoviesCategorySlider extends React.Component {
   static propTypes = {
     category: PropTypes.instanceOf(Immutable.Object).isRequired,
   };
-
-  constructor(props) {
-    super(props);
-    this.tlIn = null;
-  }
-
-  componentDidMount() {
-    this.initTransition();
-    if (this.tlIn) {
-      this.tlIn.play();
-    }
-  }
-
-  componentDidUpdate() {
-    this.initTransition();
-    if (this.tlIn) {
-      this.tlIn.play();
-    }
-  }
-
-
-  initTransition() {
-    //Detect mobile
-    const ua = navigator.userAgent;
-    this.isMobileWebkit = /WebKit/.test(ua) && /Mobile/.test(ua);
-    if (this.isMobileWebkit) return;
-
-    this.container = React.findDOMNode(this.refs.slContainer);
-
-    if (!this.container.children) return;
-
-    this.tlIn = new TimelineMax({paused: true});
-
-    this.tlIn.add(TweenMax.staggerFromTo(this.container.children, 0.2, {
-      autoAlpha: 0,
-      y: 30
-    }, {
-      autoAlpha: 1,
-      y: 0,
-      ease: Sine.easeOut
-    }, 0.03));
-  }
 
   render() {
     const {
