90386712e9c11365111dc7d035c1f93d9274fdb7
benjipott <pott.benjamin@gmail.com>
2015-12-20 14:41:05 +0100

    add alert message from config

--- config/client.js
+++ config/client.js
@@ -113,6 +113,9 @@ const config = {
       price: '5999'
     }
   ],
+  alerts: [
+    'L\' application android est indisponible pour le moment pour un problème indépendant de notre volonté. Nous faisons le maximum pour un retour rapide à la normale.'
+  ],
   google: {
     analyticsKey: process.env.GA_TRACKING_ID || 'UA-*******-**'
   },
---
+++ src/js/components/Alert/AlertMessage.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import {canUseDOM} from 'react/lib/ExecutionEnvironment';
+import config from '../../../../config';
+import CookieMessage from '../Welcome/WelcomeComponents/CookieMessage';
+
+if (process.env.BROWSER) {
+  require('./AlertMessage.less');
+}
+
+class AlertMessage extends React.Component {
+
+  // do not render cookie message on server-side
+  state = {
+    isCookieSet: '1'
+  };
+
+  componentDidMount() {
+    let isCookieAccepted = this.isCookieAccepted();
+
+    if (isCookieAccepted !== '1') {
+      this.setState({isCookieSet: '0'});
+    }
+  }
+
+  isCookieAccepted() {
+    let isCookieAccepted = null;
+
+    if (canUseDOM) {
+      isCookieAccepted = localStorage.getItem('afrostreamAlert');
+    }
+    return isCookieAccepted;
+  }
+
+  setCookieToken() {
+    if (canUseDOM) {
+      localStorage.setItem('afrostreamAlert', '1');
+      this.setState({isCookieSet: '1'});
+    }
+  }
+
+  render() {
+
+    return (
+      <div className="alerts">
+        {this.state.isCookieSet === '1' ? <div /> : <div className="alert-message">
+          {config.alerts ? config.alerts.map((alert, i) => <span
+            key={`alert-${i}`}>{alert}</span>) : ''}
+          <button className="alert-button" onClick={::this.setCookieToken} onTouchEnd={::this.setCookieToken}>OK
+          </button>
+        </div> }
+        <CookieMessage />
+      </div>
+    );
+  }
+}
+
+export default AlertMessage;
