29691febce9e636b32925b46005ccb941bca6702
benjipott <pott.benjamin@gmail.com>
2016-01-08 13:35:41 +0100

    fix modal

--- src/js/actions/modal.js
+++ src/js/actions/modal.js
@@ -1,19 +1,15 @@
 import ActionTypes from '../consts/ActionTypes';
 
 export function open(target) {
-  return (dispatch, getState) => {
-    return {
-      type: ActionTypes.Modal.open,
-      target
-    };
+  return {
+    type: ActionTypes.Modal.open,
+    target
   };
 }
 
 export function close() {
-  return (dispatch, getState) => {
-    return {
-      type: ActionTypes.Modal.close,
-      target: null
-    };
+  return {
+    type: ActionTypes.Modal.close,
+    target: null
   };
 }
