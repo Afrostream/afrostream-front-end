1ede9488df1fae60bb3bd8d4264ddf8d99e8b763
benjipott <pott.benjamin@gmail.com>
2016-02-03 11:07:04 +0100

    fix size share image

--- src/js/decorators/metasData.js
+++ src/js/decorators/metasData.js
@@ -91,7 +91,7 @@ export default () => {
         if (poster) {
           imageStyle = poster.get('imgix');
         }
-        let ogImage = `${imageStyle}?crop=faces&fit=clip&w=1920&h=815&q=${config.images.quality}&fm=${config.images.type}`;
+        let ogImage = `${imageStyle}?crop=faces&fit=clip&w=1120&h=630&q=${config.images.quality}&fm=${config.images.type}`;
 
         if (title) {
           metas.title = title;
