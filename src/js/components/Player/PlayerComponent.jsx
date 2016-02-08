2e3d06f9d510e69f49b5b792d9ceab39c6b43d17
benjipott <pott.benjamin@gmail.com>
2016-02-08 16:53:18 +0100

    add infos season on plyer

--- src/js/components/Player/PlayerComponent.jsx
+++ src/js/components/Player/PlayerComponent.jsx
@@ -541,15 +541,22 @@ class PlayerComponent extends Component {
   render() {
     const {
       props: {
-        Video,
+        Event,
+        Season,
         Movie,
-        videoId,
+        Video,
+        seasonId,
         movieId,
-        Event
+        videoId
         }
       } = this;
 
     const hiddenMode = !Event.get('userActive');
+    let playerClasses = {
+      'player': true,
+      'player-next-reco': this.state.nextReco
+    };
+
     let videoInfoClasses = {
       'video-infos': true,
       'video-infos-hidden': hiddenMode
@@ -562,18 +569,24 @@ class PlayerComponent extends Component {
     let captions = videoData.get('captions');
     let movieData = Movie.get(`movies/${movieId}`);
     let episodeData = videoData.get('episode');
+    let seasonData = Season.get(`seasons/${seasonId}`);
     let videoDuration = this.formatTime(this.state.duration || (movieData ? movieData.get('duration') : 0));
     //si on a les données de l'episode alors, on remplace les infos affichées
     let infos = episodeData ? _.merge(episodeData.toJS() || {}, movieData.toJS() || {}) : movieData.toJS();
+    if (seasonData) {
+      infos.seasonNumber = seasonData.get('seasonNumber');
+    }
     let renderData = episodeData ? episodeData : movieData;
     return (
-      <div className="player">
+      <div className={classSet(playerClasses)}>
         <div ref="wrapper" className="wrapper"/>
         {
           movieData ?
             <div className={classSet(videoInfoClasses)}>
               <div className=" video-infos_label">Vous regardez</div>
               <div className=" video-infos_title">{infos.title}</div>
+              {infos.seasonNumber ?
+                <div className=" video-infos_episode">{`Saison ${infos.seasonNumber}`}</div> : ''}
               {infos.episodeNumber ?
                 <div className=" video-infos_episode">{`Episode ${infos.episodeNumber}`}</div> : ''}
               <FavoritesAddButton data={renderData} dataId={renderData.get('_id')}/>
