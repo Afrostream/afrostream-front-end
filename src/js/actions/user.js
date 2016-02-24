5db9c5624161b7bfaa45b121c935fa3a0368e12e
benjipott <pott.benjamin@gmail.com>
2016-02-24 15:19:42 +0100

    billing : subscribe via recurly

--- src/js/actions/user.js
+++ src/js/actions/user.js
@@ -70,10 +70,10 @@ const mergeProfile = function (data, getState, actionDispatcher) {
 export function subscribe(data, isGift = false) {
   return (dispatch, getState) => {
     //FIXME use only billings route when all recurly dependency was removed from bo to billing project
-    if (data.billingProvider === 'recurly') {
+    if (isGift && data.billingProvider === 'recurly') {
       return async api => ({
         type: ActionTypes.User.subscribe,
-        res: await api(`/api/subscriptions${isGift ? '/gift' : '' }`, 'POST', data),
+        res: await api(`/api/subscriptions/gift`, 'POST', data),
         isGift
       });
     }
