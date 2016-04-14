e3c590cbdd3f402974e4a5da730a27b4da22f18f
benjipott <pott.benjamin@gmail.com>
2016-04-14 18:56:31 +0200

    plans home skin

--- src/js/components/Welcome/WelcomeComponents/PricingTable.jsx
+++ src/js/components/Welcome/WelcomeComponents/PricingTable.jsx
@@ -74,11 +74,18 @@ class PricingTable extends React.Component {
         case 'price':
           value = `${formatPrice(plan.get('amountInCents'), plan.get('currency'), true)}/${plan.get('periodLength')}${dict.account.billing.periods[plan.get('periodUnit')]}`;
           break;
+        case 'internalMaxScreens':
+          value = `${objVal} ${dict.planCodes.infos[label]}`;
+          break;
         default :
           let isBool = (objVal === 'true' || objVal === 'false' || typeof objVal === 'boolean' ) && typeof isBoolean(objVal) === 'boolean';
-          value = objVal;
-          if (isBool && isBoolean(objVal)) {
-            value = dict.planCodes.infos[label] || '';
+          if (isBool) {
+            if (isBoolean(objVal)) {
+              value = dict.planCodes.infos[label] || '';
+            }
+          }
+          else {
+            value = objVal;
           }
           break;
       }
@@ -108,8 +115,10 @@ class PricingTable extends React.Component {
       let key = plan.get('internalPlanUuid');
 
       return (
-        <div key={`col-plan-${key}`} className="col col-xs-4 col-sm-4 col-md-2 col-md-offset-1">
-          {this.getPlanRow(plan)}
+        <div key={`col-plan-${key}`} className="col col-xs-12 col-sm-12 col-md-2">
+          <div className="plan-container">
+            {this.getPlanRow(plan)}
+          </div>
         </div>);
     });
   }
@@ -135,100 +144,11 @@ class PricingTable extends React.Component {
   }
 
   render () {
-
-
     return (
       <section className="pricing">
         <div className="row">
-
           {this.getFirstCol()}
           {this.getPlanCol()}
-
-        </div>
-
-        <div className="pricing-table">
-          <div className="pricing-header-element">
-            <h1>
-              <span className="pricing-header-purple">Nos formules </span>
-            </h1>
-            <div className="pricing-baseline">Service uniquement disponible en France, DOM-TOM,
-              Belgique, Luxembourg, Suisse, Sénégal, Côte d'Ivoire.
-              <Link to="/faq">Les réponses à vos questions</Link>
-            </div>
-          </div>
-          <div className="pricing-plans">
-
-            <div className="pricing-plan">
-              <img className="plan-image" src="/images/titre_abo_1.png" alt="Formule"/>
-
-              <h3 className="plan-heading plan1-heading">MENSUEL</h3>
-
-              <div className="plan-details-1">
-                <div className="prices">
-                  <span className="price">6,99€</span>
-                  <span className="period">/MOIS</span>
-                </div>
-                <div className="plan-highlight plan1-highlight">Prélèvement mensuel</div>
-                <ul className="plan-benefits-1">
-                  <li>Une semaine d'éssai</li>
-                  <li>Sans engagement</li>
-                  <li>1 seul écran connecté</li>
-                  <li>Films et séries illimités</li>
-                </ul>
-              </div>
-              <button className="btn-plan plan1-button" value="afrostreammonthly" onClick={::this.openModal}>
-                DÉMARREZ 7 JOURS D'ESSAI
-              </button>
-
-            </div>
-
-            <div className="pricing-plan">
-              <img className="plan-image" src="/images/titre_abo_2.png" alt="Formule"/>
-
-              <h3 className="plan-heading plan2-heading">SÉRÉNITÉ</h3>
-
-              <div className="plan-details-2">
-                <div className="prices">
-                  <span className="price">59,99€</span>
-                  <span className="period">/AN</span>
-                </div>
-                <ul className="plan-benefits-2">
-                  <li>Une semaine d'éssai</li>
-                  <li>2 écrans connectés simultanément</li>
-                  <li>Invitations VIP aux avant-premières de films</li>
-                  <li>12 mois inclus</li>
-                  <li>Films et séries illimités</li>
-                </ul>
-              </div>
-              <button className="btn-plan plan2-button" value="afrostreamambassadeurs" onClick={::this.openModal}>
-                DÉMARREZ 7 JOURS D'ESSAI
-              </button>
-
-            </div>
-
-            <div className="pricing-plan">
-              <img className="plan-image" src="/images/titre_abo_3.png" alt="Formule"/>
-
-              <h3 className="plan-heading plan3-heading">CADEAU</h3>
-
-              <div className="plan-details-3">
-                <div className="prices">
-                  <span className="price">59,99€</span>
-                  <span className="period">/AN</span>
-                </div>
-                <ul className="plan-benefits-1">
-                  <li>2 écrans connectés simultanément</li>
-                  <li>Invitations VIP aux avant-premières de films</li>
-                  <li>12 mois inclus</li>
-                  <li>Films et séries illimités</li>
-                </ul>
-              </div>
-              <button className="btn-plan plan3-button" value="afrostreamgift" onClick={::this.openModal}>
-                OFFRIR UN ABONNEMENT
-              </button>
-
-            </div>
-          </div>
         </div>
       </section>
     );
