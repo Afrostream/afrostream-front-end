e7f94ed9f4d4ff9f629cfdcd63f4bfb65cbecef4
benjipott <pott.benjamin@gmail.com>
2016-05-31 11:23:22 +0200

    babel-register es5

--- server/index.js
+++ server/index.js
@@ -1,12 +1,9 @@
-import cluster from 'express-cluster'
-import heapdump from 'heapdump'
+require('babel-register')
+var cluster = require('express-cluster')
 
-let clusterConf = {count: process.env.WEB_CONCURRENCY || 1, verbose: true}
+var clusterConf = {count: process.env.WEB_CONCURRENCY || 1, verbose: true}
 
 cluster(function (worker) {
   console.log('worker ' + worker.id + ' is up')
-  heapdump.writeSnapshot((err, filename) => {
-    console.log('dump written to', filename)
-  })
   return require('./app')
 }, clusterConf)
