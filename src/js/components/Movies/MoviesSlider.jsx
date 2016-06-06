4883e792e83d2b2a96aa2a2e5cac78d296185993
benjipott <pott.benjamin@gmail.com>
2016-06-06 13:00:46 +0200

    Scroll virtualized (#444)
    * try react-virtualized
    
    * slider : refacto

--- src/js/components/Movies/MoviesSlider.jsx
+++ src/js/components/Movies/MoviesSlider.jsx
@@ -1,15 +1,24 @@
 import React, { PropTypes } from 'react'
 import Immutable from 'immutable'
 import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment'
-import Slider from '../Slider/Slider'
+import { ArrowStepper } from '../Slider'
 import Thumb from '../Movies/Thumb'
 import ReactList from 'react-list'
+import { AutoSizer, ColumnSizer, VirtualScroll, Grid, Collection } from 'react-virtualized'
 
 if (process.env.BROWSER) {
   require('./MoviesSlider.less')
 }
 
+const GUTTER_SIZE = 3
+const CELL_WIDTH = 160
+
 class MoviesSlider extends React.Component {
+
+  constructor (props, context) {
+    super(props, context)
+    this._columnYMap = []
+  }
 
   static propTypes = {
     dataList: PropTypes.instanceOf(Immutable.List),
@@ -17,7 +26,8 @@ class MoviesSlider extends React.Component {
     label: React.PropTypes.string,
     slug: React.PropTypes.string,
     axis: React.PropTypes.string,
-    className: React.PropTypes.string
+    className: React.PropTypes.string,
+    rowHeight: React.PropTypes.number
   }
 
   static defaultProps = {
@@ -25,8 +35,38 @@ class MoviesSlider extends React.Component {
     label: '',
     slug: '',
     axis: 'x',
-    className: 'movies-data-list'
+    className: 'movies-data-list',
+    rowHeight: 200
   }
+
+  cellSizeAndPositionGetter ({index}) {
+    const {
+      props: {
+        dataList
+      }
+    } = this
+
+    let data = dataList.get(index)
+    let columnCount = 6
+    const columnPosition = index % (columnCount || 1)
+    const datum = dataList.get(index % data.size)
+
+    // Poor man's Masonry layout; columns won't all line up equally with the bottom.
+    const height = 220
+    const width = CELL_WIDTH
+    const x = columnPosition * (GUTTER_SIZE + width)
+    const y = this._columnYMap[columnPosition] || 0
+
+    this._columnYMap[columnPosition] = y + height + GUTTER_SIZE
+
+    return {
+      height,
+      width,
+      x,
+      y
+    }
+  }
+
 
   renderBlock (data) {
     let isAdSpot = data.get('adSpot')
@@ -52,7 +92,7 @@ class MoviesSlider extends React.Component {
     )
   }
 
-  renderItem (index) {
+  getRowHeight ({index}) {
     const {
       props: {
         dataList
@@ -61,10 +101,32 @@ class MoviesSlider extends React.Component {
 
     let data = dataList.get(index)
     if (data instanceof Immutable.Map) {
+      return 240
+    }
+    return 465
+  }
+
+  renderIndex (index) {
+    return this.renderItem({columnIndex: index})
+  }
+
+  renderCell ({index}) {
+    return this.renderItem({columnIndex: index})
+  }
+
+  renderItem ({columnIndex}) {
+    const {
+      props: {
+        dataList
+      }
+    } = this
+
+    let data = dataList.get(columnIndex)
+    if (data instanceof Immutable.Map) {
       return this.renderBlock(data)
     }
     return (
-      <div className="block" key={`data-block-${index}`}>{data.map((item)=> {
+      <div className="block" key={`data-block-${columnIndex}`}>{data.map((item)=> {
         return this.renderBlock(item)
       })}</div>
     )
@@ -76,7 +138,9 @@ class MoviesSlider extends React.Component {
         dataList,
         selectedId,
         label,
-        slug
+        slug,
+        axis,
+        rowHeight
       }
     } = this
 
@@ -93,23 +157,58 @@ class MoviesSlider extends React.Component {
       })
     }
 
+    if (axis === 'y') {
+      return (
+        <div className={this.props.className}>
+          {slug ? <div id={slug} className="movies-list__anchor"/> : ''}
+          {label ? <div className="movies-list__selection">{label}</div> : ''}
+          <div className="slider-container">
+            <ReactList
+              ref="react-list"
+              initialIndex={index}
+              axis={this.props.axis}
+              itemRenderer={::this.renderIndex}
+              length={dataList.size}
+              type={'uniform'}
+            />
+          </div>
+        </div>
+      )
+    }
     return (
       <div className={this.props.className}>
         {slug ? <div id={slug} className="movies-list__anchor"/> : ''}
         {label ? <div className="movies-list__selection">{label}</div> : ''}
-        <Slider {...this.props}>
-          <div className="slider-container">
-            <ReactList
+        <AutoSizer className="slider-container" disableHeight>
+          {({width}) => (
+            <ColumnSizer
               ref="react-list"
-              useTranslate3d={true}
-              initialIndex={index}
-              axis={this.props.axis}
-              itemRenderer={::this.renderItem}
-              length={dataList.size}
-              type={this.props.axis === 'x' ? 'variable' : 'uniform' }
-            />
-          </div>
-        </Slider>
+              columnMaxWidth={210}
+              columnMinWidth={210}
+              columnCount={dataList.size}
+              width={width}>
+              {({adjustedWidth, getColumnWidth, registerChild}) => (
+                <ArrowStepper columnCount={dataList.size}>
+                  {({onScroll, columnCount, scrollLeft}) => (
+                    <Grid
+                      ref={registerChild}
+                      cellRenderer={::this.renderItem}
+                      columnWidth={getColumnWidth}
+                      columnCount={columnCount}
+                      rowHeight={rowHeight}
+                      height={rowHeight}
+                      rowCount={1}
+                      onScroll={onScroll}
+                      scrollLeft={scrollLeft}
+                      width={adjustedWidth}
+                      scrollToColumn={index}
+                    />
+                  )}
+                </ArrowStepper>
+              )}
+            </ColumnSizer>
+          )}
+        </AutoSizer>
       </div>
     )
   }
