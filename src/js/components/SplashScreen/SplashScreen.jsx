c3a7da2f9573d893dc5cfb8c6a92d90af5cca766
benjipott <pott.benjamin@gmail.com>
2016-08-08 17:25:05 +0200

    bump react

--- src/js/components/SplashScreen/SplashScreen.jsx
+++ src/js/components/SplashScreen/SplashScreen.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
 import { canUseDOM } from 'fbjs/lib/ExecutionEnvironment'
+import ReactDOM from'react-dom'
+import { connect } from 'react-redux'
 import config from '../../../../config'
+import classNames from 'classnames'
+import { isBoolean } from '../../lib/utils'
 
 if (process.env.BROWSER) {
   require('./SplashScreen.less')
 }
 
+@connect(({User}) => ({User}))
 class SplashScreen extends React.Component {
 
   // do not render cookie message on server-side
   state = {
     isCookieSet: '1',
-    splash: 1
+    splash: 0
   }
+
+  timeoutSplash = 0
 
   componentDidMount () {
     let isCookieAccepted = this.isCookieAccepted()
@@ -21,9 +28,18 @@ class SplashScreen extends React.Component {
       this.setState({splash: 0})
     }
 
-    setTimeout(()=> {
-      this.setState({splash: 1})
-    }, 5000)
+    this.timeoutSplash = setTimeout(()=> {
+      this.hideSplash()
+    }, 10000)
+
+    const elTarget = ReactDOM.findDOMNode(this)
+    elTarget.addEventListener('mousewheel', ::this.hideSplash, false)
+    elTarget.addEventListener('DOMMouseScroll', ::this.hideSplash, false)
+  }
+
+  hideSplash () {
+    clearTimeout(this.timeoutSplash)
+    this.setState({splash: 1})
   }
 
   isCookieAccepted () {
@@ -43,20 +59,38 @@ class SplashScreen extends React.Component {
   }
 
   getSplash (i, splash) {
-    if (!splash || this.state.splash) {
-      return ''
+    if (!splash || isBoolean(splash.get('showed'))) {
+      return
     }
+
+    let imageStyle = {backgroundImage: `url(${splash.get('poster')}?crop=faces&fit=clip&w=1080&q=${config.images.quality}&fm=${config.images.type})`}
+
+    let splashClass = {
+      'splash': true,
+      'slide-top': this.state.splash
+    }
+
     return (
-      <div className="splash" key={`splash-${i}`}>
-        <h1>{splash.label}</h1>
-      </div>
+      <a href={splash.link} onClick={::this.hideSplash}>
+        <div className={classNames(splashClass)} key={`splash-${i}`} style={imageStyle}>
+        </div>
+      </a>
     )
   }
 
   render () {
+    const {
+      props: {User}
+    } = this
+
+    const user = User.get('user')
+    if (!user) {
+      return <div />
+    }
+    const splashs = user.get('splashs')
     return (
       <div className="splash-screen">
-        {config.splashs ? config.splashs.map((splash, i) => this.getSplash(i, splash)) : ''}
+        {splashs && splashs.map((splash, i) => this.getSplash(i, splash))}
       </div>
     )
   }
