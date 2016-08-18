77c5f5736bb763b6472e32e4d174fdbf35a5ea2f
benjipott <pott.benjamin@gmail.com>
2016-08-18 17:22:50 +0200

    bump player

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -80,8 +80,7 @@ const webpackConfig = {
   resolve: {
     extensions: ['', '.js', '.jsx', '.json'],
     alias: {
-      jquery: path.join(__dirname, '../node_modules/jquery/dist/jquery'),
-      videojs: path.join(__dirname, '../node_modules/video.js/dist/video.js')
+      jquery: path.join(__dirname, '../node_modules/jquery/dist/jquery')
     }
   },
   stats: {
@@ -134,10 +133,11 @@ const webpackConfig = {
       },
       {
         test: /vtt\.js$/,
-        loader: 'url-loader?name=[name].[ext]?[hash]&limit=10000'
+        loader: 'url-loader?name=[name].[ext]?[hash]&limit=10000',
+        include: [path.join(__dirname, '../node_modules/afrostream-player')]
       },
       {
-        test: /video\.js/,
+        test: /video\.js$/,
         loader: 'expose?videojs',
         include: [path.join(__dirname, '../node_modules/afrostream-player')]
       },
@@ -174,7 +174,6 @@ const webpackConfig = {
     new webpack.ContextReplacementPlugin(/moment\.js[\/\\]locale$/, /^\.\/(fr|en)$/),
     new ExtractTextPlugin({filename: '[name].css', allChunks: true}),
     new webpack.ProvidePlugin({
-      videojs: 'video.js',
       sendBirdClient: 'sendbird',
       $: 'jquery',
       jQuery: 'jquery',
