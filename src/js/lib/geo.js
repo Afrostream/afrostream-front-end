3252e727ba632a2949d0d614c7b5889ea2c7aac2
benjipott <pott.benjamin@gmail.com>
2016-08-19 15:14:21 +0200

    filtering plans by user and country

--- src/js/lib/geo.js
+++ src/js/lib/geo.js
@@ -22,3 +22,24 @@ export async function isAuthorized () {
       })
   })
 }
+
+export async function getCountry () {
+  return await new Promise((resolve, reject) => {
+    const url = protocol + '://' + authority + '/auth/geo'
+
+    request
+      .get(url)
+      .type('json')
+      .end((err, response) => {
+        if (err) {
+          return reject(err)
+        }
+        if (response && response.body &&
+          response.body.countryCode) {
+          return resolve(response.body.countryCode)
+        }
+        // undefined, error, or true => true.
+        resolve('FR')
+      })
+  })
+}
