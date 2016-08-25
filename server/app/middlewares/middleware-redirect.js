a6ee90cc6e5b8840649430791b668cf314200f92
benjipott <pott.benjamin@gmail.com>
2016-08-25 10:26:06 +0200

    fix redirects

--- server/app/middlewares/middleware-redirect.js
+++ server/app/middlewares/middleware-redirect.js
@@ -3,15 +3,19 @@ import config from '../../../config'
 export default function forceSSL () {
   return function (req, res, next) {
     if (req.headers['x-forwarded-proto'] === 'https') {
-      return next();
+      return next()
     }
     res.redirect('https://' + config.domain.host + req.url)
   }
 }
 export default function forceWWW () {
   return function (req, res, next) {
+    const env = process.env.NODE_ENV || 'development'
+    if (env === 'development') {
+      return next()
+    }
     if (req.host.indexOf('www.') !== 0) {
-      res.redirect(301, req.protocol + '://www.' + config.domain.host + req.url)
+      return res.redirect(301, req.protocol + '://www.' + config.domain.host + req.url)
     }
     next()
   }
