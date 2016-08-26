a4a3556de495c466ae9fab13d0f36f8baeed01d3
benjipott <pott.benjamin@gmail.com>
2016-08-26 09:56:26 +0200

    redirect www

--- server/app/app.js
+++ server/app/app.js
@@ -26,6 +26,7 @@ function errorHandler (err, req, res, next) {
 app.set('startDate', new Date())
 //
 app.use(forceSSL())
+app.use(forceWWW())
 // We point to our static assets
 app.use(compression())
 //
--- server/app/middlewares/middleware-redirect.js
+++ server/app/middlewares/middleware-redirect.js
@@ -1,7 +1,11 @@
 import config from '../../../config'
+const env = process.env.NODE_ENV || 'development'
 
 export function forceSSL () {
   return function (req, res, next) {
+    if (env === 'development') {
+      return next()
+    }
     const proto = req.get('x-forwarded-proto')
 
     if (!proto || proto === 'https') {
@@ -12,12 +16,11 @@ export function forceSSL () {
 }
 export function forceWWW () {
   return function (req, res, next) {
-    const env = process.env.NODE_ENV || 'development'
     if (env === 'development') {
       return next()
     }
     if (req.host.indexOf('www.') !== 0) {
-      return res.redirect(301, req.protocol + '://www.' + config.domain.host + req.url)
+      return res.redirect(301, req.protocol + '://www.' + config.domain.host + req.originalUrl)
     }
     next()
   }
