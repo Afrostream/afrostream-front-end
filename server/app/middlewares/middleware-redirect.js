009d02daca934ccc5eaae2971747e2c009eed73c
Marc Dassonneville <marc.dassonneville@gmail.com>
2016-08-29 11:44:15 +0200

    middleware: redirect: avoid redirect on /auth/* routes (bouygues/orange/facebook)

--- server/app/middlewares/middleware-redirect.js
+++ server/app/middlewares/middleware-redirect.js
@@ -18,9 +18,14 @@ export function forceWWW () {
   return function (req, res, next) {
     const proto = req.get('x-forwarded-proto') || req.protocol
     const cdnHostname = req.get('x-afsm-forwarded-host')
-    if (cdnHostname && !cdnHostname.match(/^www\./i)) {
-      return res.redirect(301, proto + '://www.' + config.domain.host + req.originalUrl)
+
+    if (!cdnHostname ||                         // no header x-afsm-forwarded-host <=> env=dev|staging
+        req.originalUrl.match(/^\/auth\//i) ||  // auth facebook, orange, bouygues
+        cdnHostname.match(/^www\./i)) {         // already redirected
+      // skipping redirect
+      return next()
     }
-    next()
+    // redirect on www.afrostream.tv
+    return res.redirect(301, proto + '://www.' + config.domain.host + req.originalUrl)
   }
 }
