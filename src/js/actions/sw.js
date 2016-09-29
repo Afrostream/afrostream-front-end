6041095b541d00e0ec1faedf1e5d41c47fdae813
benjipott <pott.benjamin@gmail.com>
2016-09-29 15:16:38 +0200

    update notification method

--- src/js/actions/sw.js
+++ src/js/actions/sw.js
@@ -7,34 +7,46 @@ export function setPushNotifications (value) {
     return async () => {
       //console.log('service Worker ready ', navigator.serviceWorker.ready)
 
-      return await navigator.serviceWorker.ready
-        .then((reg)=> {
-          swReg = reg
-          return swReg.pushManager.permissionState({userVisibleOnly: true})
+      return await navigator.serviceWorker.register('sw.js')
+      //return await navigator.serviceWorker.ready
+        .then((registration)=> {
+          swReg = registration
+          //return swReg.pushManager.permissionState({userVisibleOnly: true})
+          return swReg.pushManager.getSubscription()
         })
-        .then((state)=> {
-          if (value && (!state || state === 'prompt')) {
-            return swReg.pushManager.subscribe({userVisibleOnly: true})
-          } else {
-            return swReg.pushManager.getSubscription({userVisibleOnly: true})
+        //.then((state)=> {
+        //  if (value && (!state || state === 'prompt')) {
+        //    return swReg.pushManager.subscribe({userVisibleOnly: true})
+        //  } else {
+        //    return swReg.pushManager.getSubscription({userVisibleOnly: true})
+        //  }
+        //})
+        .then((subscription)=> {
+          if (subscription) {
+            return subscription
           }
-        }).then((subscription)=> {
+          return swReg.pushManager.subscribe({userVisibleOnly: true})
+        })
+        .then((subscription)=> {
           console.log('Subscription Notification', subscription)
           if (subscription) {
-            return async api=> {
-              return await
-                api({
-                  path: `/api/subscriptions`,
-                  method: 'post',
-                  passToken: true,
-                  params: subscription
-                }).then((result)=> {
-                  return {
-                    type: ActionTypes.SW.setPushNotifications,
-                    result
-                  }
-                })
-            }
+            console.log('Subscription endpoint', subscription.endpoint)
+            //return async api => {
+            //  return await
+            //    api({
+            //      path: `/api/users`,
+            //      method: 'post',
+            //      passToken: true,
+            //      params: {
+            //        endpoint: subscription.endpoint
+            //      }
+            //    }).then((result)=> {
+            //      return {
+            //        type: ActionTypes.SW.setPushNotifications,
+            //        result
+            //      }
+            //    })
+            //}
           }
           return {
             type: ActionTypes.SW.setPushNotifications,
