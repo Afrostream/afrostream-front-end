1dcff62a0f83083e6a307e16e74ef664c2382029
benjipott <pott.benjamin@gmail.com>
2016-09-29 13:09:31 +0200

    subscription subscribe return null

--- src/js/actions/sw.js
+++ src/js/actions/sw.js
@@ -5,26 +5,44 @@ export function setPushNotifications (value) {
   return (dispatch, getState, actionDispatcher) => {
     let swReg
     return async () => {
-      return await navigator.serviceWorker.getRegistration()
+      //console.log('service Worker ready ', navigator.serviceWorker.ready)
+
+      return await navigator.serviceWorker.ready
         .then((reg)=> {
           swReg = reg
           return swReg.pushManager.permissionState({userVisibleOnly: true})
         })
         .then((state)=> {
           if (value && (!state || state === 'prompt')) {
-            debugger
             return swReg.pushManager.subscribe({userVisibleOnly: true})
           } else {
             return swReg.pushManager.getSubscription({userVisibleOnly: true})
           }
-        }).then(()=> {
+        }).then((subscription)=> {
+          console.log('Subscription Notification', subscription)
+          if (subscription) {
+            return async api=> {
+              return await
+                api({
+                  path: `/api/subscriptions`,
+                  method: 'post',
+                  passToken: true,
+                  params: subscription
+                }).then((result)=> {
+                  return {
+                    type: ActionTypes.SW.setPushNotifications,
+                    result
+                  }
+                })
+            }
+          }
           return {
             type: ActionTypes.SW.setPushNotifications,
             value
           }
         }).catch((err)=> {
-          actionDispatcher(EventActionCreators.showError(err))
           console.log(err)
+          actionDispatcher(EventActionCreators.showError(err))
         })
     }
   }
