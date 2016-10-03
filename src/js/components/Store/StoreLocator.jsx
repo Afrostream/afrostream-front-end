309550a401d7941e7355ce789f7345693353f31c
benjipott <pott.benjamin@gmail.com>
2016-10-03 17:37:18 +0200

    add store locator map

--- src/js/components/Store/StoreLocator.jsx
+++ src/js/components/Store/StoreLocator.jsx
@@ -1,11 +1,8 @@
 import React, { PropTypes } from 'react'
-import SignUpButton from '../User/SignUpButton'
 import { prepareRoute } from '../../decorators'
 import config from '../../../../config'
-import * as ConfigActionCreators from '../../actions/config'
 import * as EventActionCreators from '../../actions/event'
 import scriptLoader from '../../lib/script-loader'
-import Player from '../Player/Player'
 import { withRouter } from 'react-router'
 
 const {gmapApi} = config
@@ -16,11 +13,9 @@ if (process.env.BROWSER) {
 
 @prepareRoute(async function ({store}) {
   return await Promise.all([
-    store.dispatch(EventActionCreators.pinHeader(true)),
-    store.dispatch(ConfigActionCreators.getConfig('store'))
+    store.dispatch(EventActionCreators.pinHeader(true))
   ])
 })
-@connect(({Config}) => ({Config}))
 class StoreLocator extends React.Component {
 
   state = {
@@ -36,12 +31,40 @@ class StoreLocator extends React.Component {
   }
 
   initMap () {
+
     const map = new google.maps.Map(document.getElementById('map-canvas'), {
-      zoom: 2,
+      zoom: 6,
       center: new google.maps.LatLng(2.8, -187.3),
       mapTypeId: 'terrain'
     })
-    map.data.loadGeoJson('stores')
+
+    this.geoCoder = new google.maps.Geocoder
+
+    google.maps.event.addListener(map, 'idle', () => {
+      return I ? void(I = !1) : (map.data.forEach((e)=> {
+        map.data.remove(e)
+      }), void map.data.loadGeoJson(config.apiClient.urlPrefix + '/api/stores?latitude=' + map.getBounds().getCenter().lat + '&longitude=' + map.getBounds().getCenter().ln + '&distance=' + map.getZoom()))
+    })
+
+    map.data.loadGeoJson('https://afr-api-v1-staging.herokuapp.com/api/stores')
+  }
+
+  searchLocation () {
+    const e = document.getElementById('map-search')
+    this.geoCoder.geocode({
+      address: e.value,
+      region: 'FR'
+    }, (e, t) => {
+      //t == google.maps.GeocoderStatus.OK ? (
+      //  const f = e[0].types[0];
+      //  const m = {
+      //    lat: e[0].geometry.location.lat(),
+      //    lng: e[0].geometry.location.lng()
+      //  }, c
+      //  ()
+      //  ) :
+      //  l()
+    })
   }
 
   componentDidMount () {
@@ -49,14 +72,14 @@ class StoreLocator extends React.Component {
       props: {isScriptLoaded, isScriptLoadSucceed}
     } = this
     if (isScriptLoaded && isScriptLoadSucceed) {
-      initMap()
+      this.initMap()
     }
   }
 
   componentWillReceiveProps ({isScriptLoaded, isScriptLoadSucceed}) {
     if (isScriptLoaded && !this.props.isScriptLoaded) { // load finished
       if (isScriptLoadSucceed) {
-        initMap()
+        this.initMap()
       }
     }
   }
@@ -75,8 +98,7 @@ class StoreLocator extends React.Component {
 
     return (
       <div className="store-locator-page">
-        <div className="container brand-bg-alpha">
-          <Player src={source} options={{autoplay: false}}/>
+        <div className="container-fluid brand-bg-alpha">
           <section className="cashway-info">
             <h2>Abonnez-vous à Afrostream au coin de la rue</h2>
             <p>Il n’a jamais été aussi simple de s’abonner à Afrostream.
@@ -84,53 +106,6 @@ class StoreLocator extends React.Component {
               Afrostream.
               Trouvez le bureau de tabac le plus proche de chez vous grâce à cette carte et profitez des meilleurs films
               et séries afro.</p>
-            <h3>Comment ça marche</h3>
-            <div className="row-fluid">
-              <div className="col-xs-12 col-md-4">
-                <div className='number'>1</div>
-                <img className="img-responsive img-center"
-                     src="/images/payment/cashway/step-1.jpg" alt="code_bare_ccm"/>
-                <div className="row-fluid">
-                  <div className="col-md-12">
-                    <div className="container_title">Le commerçant scanne votre code-barre</div>
-                  </div>
-                </div>
-
-              </div>
-              <div className="col-xs-12 col-md-4">
-                <div className='number'>2</div>
-                <img className="img-responsive img-center"
-                     src="/images/payment/cashway/step-2.jpg" alt="money_ccm"/>
-                <div className="row-fluid">
-                  <div className="col-md-12">
-                    <div className="container_title">Vous payez en espèces</div>
-                  </div>
-                </div>
-
-              </div>
-              <div className="col-xs-12 col-md-4">
-                <div className='number'>3</div>
-                <img className="img-responsive img-center"
-                     src="/images/payment/cashway/step-3.jpg" alt="validate_ccm"/>
-                <div className="row-fluid">
-                  <div className="col-md-12">
-                    <div className="container_title">Votre commande est validée ! Vous recevez un message de
-                      confirmation,
-                      et
-                      vous pouvez profiter du service instantanément
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-          </section>
-          <section className="cashway-signup ">
-            <div className="row-fluid">
-              <div className="col-md-12 text-center">
-                <SignUpButton label="RECEVOIR MON CODE" to="/cash/select-plan"/>
-              </div>
-            </div>
           </section>
           <section className="cashway-plan">
             <h3> Où payer en espèces ?</h3>
@@ -138,19 +113,12 @@ class StoreLocator extends React.Component {
               <span className="form-label" htmlFor="number">Trouver un point de vente</span>
             </div>
             <div className="form-group col-md-10">
-              <input type="text" id="cashway-map-search" defaultValue={this.state.location} className="form-control"/>
+              <input type="text" id="map-search" defaultValue={this.state.location} className="form-control"/>
             </div>
             <div className="form-group col-md-2">
-              <button className="btn btn-default" id="cashway-map-search-btn">Verifier</button>
+              <button className="btn btn-default" id="map-search-btn" onClick={::this.searchLocation}>Verifier</button>
             </div>
             <div id="map-canvas" className="map-canvas"></div>
-          </section>
-          <section className="cashway-signup ">
-            <div className="row-fluid">
-              <div className="col-md-12 text-center">
-                <SignUpButton label="RECEVOIR MON CODE" to="/cash/select-plan"/>
-              </div>
-            </div>
           </section>
         </div>
       </div>
