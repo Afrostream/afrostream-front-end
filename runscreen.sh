4302cb960e12cad186761775d57b9c96febb8c5c
spebsd <spebsd@gmail.com>
2016-10-24 10:29:20 +0200

    runscreen.sh / run two processes in screen (npm run watch and npm run dev)

---
+++ runscreen.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+exec >/dev/tty 2>/dev/tty </dev/tty
+
+screen -S devwatch -d -m npm run watch
+screen -S nodeapp -d -m npm run app
+
+webpackwatchPid=`screen -ls | grep "devwatch" | sed 's/	//g' | cut -f1 -d'.'`
+nodeappPid=`screen -ls | grep "nodeapp" | sed 's/	//g' | cut -f1 -d'.'`
+
+echo "npm run watch is running on screen with name webpackwatch, use ./afsm screenconn `hostname` devwatch to monitor the process..."
+echo "npm run app is running on screen with name nodeapp, use ./afsm screenconn `hostname` nodeapp to monitor the process..."
+
+while [ 1 ]
+do
+  checkdevwatch=`ps auxwww | grep "${webpackwatchPid}"`
+  checknodeapp=`ps auxwww | grep "${nodeappPid}"`
+  if [ "x${checkdevwatch}" = "x" ] || [ "x${checknodeapp}" = "x" ]
+  then
+    kill -9 ${webpackwatchPid}
+    kill -9 ${nodeappPid}
+    exit
+  fi
+  sleep 5
+done
