7c7694cf156700f01794a5b400a38d09060b6113
benjipott <pott.benjamin@gmail.com>
2016-12-09 12:07:45 +0100

    redux analitics middleware

--- src/js/lib/analytics.js
+++ src/js/lib/analytics.js
@@ -1,3 +1,21 @@
+/**
+ *
+ * @param track
+ * @param select
+ * based on https://github.com/markdalgleish/redux-analytics
+ * const action = {
+ * type: 'MY_ACTION',
+ *  meta: {
+ *   analytics: {
+ *     type: 'my-analytics-event',
+ *     payload: {
+ *       some: 'data',
+ *       more: 'stuff'
+ *     }
+ *   }
+ * }
+ *}
+ */
 export default (track, select = ({meta}) => meta.analytics) => store => next => action => {
   const returnValue = next(action)
 
