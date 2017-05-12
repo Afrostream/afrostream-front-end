e9879dec10639e2b4eacde280fdf4c38af3ce3c8
Marc Dassonneville <marc.dassonneville@gmail.com>
2017-05-12 15:38:39 +0200

    bugfix: prevent eslint error from last param & add last param for express error handler detection

--- server/app/middlewares/middleware-errorhandler.js
+++ server/app/middlewares/middleware-errorhandler.js
@@ -1,6 +1,7 @@
 export default function () {
-  return function errorHandler(err, req, res) {
+  return function errorHandler(err, req, res, next) {
     res.status(500)
     res.render('layouts/error', {error: err})
+    next // prevent es-lint error.
   }
 }
